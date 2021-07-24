5ab8cca82792dba111fd48810eda30b1be1562f2
katagata <haneimo@gmail.com>
2021-07-24 16:14:46 +0900

    php関係とvim関係のファイルを削除

--- install.sh
+++ install.sh
@@ -6,13 +6,4 @@ ln -f -s $SCRIPT_DIR/.bash_profile ~/.bash_profile
 ln -f -s $SCRIPT_DIR/.bash_aliases ~/.bash_aliases
 ln -f -s $SCRIPT_DIR/.screenrc ~/.screenrc
 mkdir -p ~/bin
-cp ./phpunit.sh ~/bin/phpunit.sh
-chmod +x ~/bin/phpunit.sh
-cp ./dusk.sh ~/bin/dusk.sh
-chmod +x ~/bin/dusk.sh
 
-mkdir ./temp
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh \
-  > ./temp/installer.sh
-# For example, we just use `~/.cache/dein` as installation directory
-sh ./temp/installer.sh ~/.cache/dein
