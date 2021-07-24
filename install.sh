e3a0b5a675709ca3f75c990ed87e0a41b91f6dff
katagata <haneimo@gmail.com>
2021-07-24 17:36:57 +0900

    ./dotfile/install.shとしたときに動作しない不具合を解消

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 cd `dirname $0`
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR=`pwd`
 ln -f -s $SCRIPT_DIR/.vimrc ~/.vimrc
 ln -f -s $SCRIPT_DIR/.bashrc ~/.bashrc
 ln -f -s $SCRIPT_DIR/.bash_profile ~/.bash_profile
 ln -f -s $SCRIPT_DIR/.bash_aliases ~/.bash_aliases
 ln -f -s $SCRIPT_DIR/.screenrc ~/.screenrc
-mkdir -p ~/bin
 
