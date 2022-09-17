d992d736fe8d8e718061f0dd694200dd33b1bb14
haneimo <haneimo@gmail.com>
2022-09-17 19:51:10 +0900

    add vim settings

--- .vimrc
+++ .vimrc
@@ -4,3 +4,8 @@ set tabstop=2
 set shiftwidth=2 
 set softtabstop=2
 set autoindent
+
+call plug#begin()
+  Plug 'prabirshrestha/vim-lsp'
+  Plug 'mattn/vim-lsp-settings'
+call plug#end()
--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ ln -f -s $SCRIPT_DIR/.bash_profile ~/.bash_profile
 ln -f -s $SCRIPT_DIR/.bash_aliases ~/.bash_aliases
 ln -f -s $SCRIPT_DIR/.screenrc ~/.screenrc
 
+
+if ! [ -e "~/.vim/autoload/plug.vim" ]; then
+  # vim plug
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
